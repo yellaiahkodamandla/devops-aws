466f4f307d1a829f0bf60d6ac9ad62b8b3fe7dca
Naresh <abbanapuri5354@gmail.com>
2021-01-07 09:55:41 +0530

    terraform

--- DevOps/Terraform-2021-Jan/Terraform-vpc-ec2-rds/main.tf
+++ DevOps/Terraform-2021-Jan/Terraform-vpc-ec2-rds/main.tf
@@ -1,6 +1,6 @@
 ## Provider aws 
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIA4WTOEALGLNEN6A44"
-  secret_key = "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"
+  access_key = "xxxxxxxxxxxxxxxxxxxxxx"
+  secret_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 } 
--- DevOps/Terraform-2021-Jan/Terraform/main.tf
+++ DevOps/Terraform-2021-Jan/Terraform/main.tf
@@ -1,6 +1,6 @@
 ## Provider aws 
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIA4WTOEALGFW6DYVML"
-  secret_key = "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"
+  access_key = "AKIA4WTOEALGFW6DxxxxxxxxxxxYVML"
+  secret_key = "d48ZHgentrMtW9qkrX+MRVGbRxxxxxxxxxxxx4Tt8vB0OCOmdX4N"
 } 
